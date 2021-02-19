ea7f9abe4f5cf20b8058250fb37cf5aa29491fd1
Rafael Camon Vílchez <rafael.camon@opensolutions.net>
2021-02-19 08:35:48 +0100

    Display total price into OrderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,7 @@ const OrderSummary = props => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
       <p>Continue to checkout?</p>
       <Button
         buttonType="Danger"
