ade252561b535ffe805ac1fa53f2689821db4168
Rafael Camon Vílchez <rafael.camon@opensolutions.net>
2021-02-19 10:03:09 +0100

    Reusing the backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,14 +6,30 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
-const Layout = props => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends React.Component {
+  state = {
+    showSideDrawer: true
+  };
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer 
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} 
+        />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+};
 
 export default Layout;
